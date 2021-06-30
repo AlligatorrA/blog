e17cbc75bda42789388463e30488461ad90e3b16
Anand <anandraj143kick@gmail.com>
2021-06-30 15:22:22 +0530

    Nested Array

--- src/App.js
+++ src/App.js
@@ -9,42 +9,67 @@ import './App.css';
 
 
 
-
-//    Handle Array with List
-function App() {
-  // const students= ["Arun", "Anand", "Aryan", "choti"];
-  //Map looping
-  const members = [
-    { name: "anand", email: "anand@g.co", roll: 2 },
-    { name: "Arun", email: "arrun@g.co", roll: 4 },
-    { name: "Aryan", email: "aryan@g.co", roll: 5 },
-    { name: "Choti", email: "choti@g.co", roll: 6 },
+    //   Nested list with Nested Array
+ function App() {
+  const students=[
+    {name:"anand", email:"anand@g.co", Address:[
+      {Hn: "10", city:"Jamui", country:"India" },
+      {Hn: "12", city:"jhajha", country:"India" },
+      {Hn: "13", city:"khairma", country:"India" },
+      {Hn: "11", city:"sono", country:"India" }
+    ]},
+    {name:"Arun", email:"arrun@g.co", Address:[
+      {Hn: "10", city:"Jamui", country:"India" },
+      {Hn: "12", city:"jhajha", country:"India" },
+      {Hn: "13", city:"khairma", country:"India" },
+      {Hn: "11", city:"sono", country:"India" }
+    ]},
+    {name:"Aryan", email:"aryan@g.co", Address:[
+      {Hn: "10", city:"Jamui", country:"India" },
+      {Hn: "12", city:"jhajha", country:"India" },
+      {Hn: "13", city:"khairma", country:"India" },
+      {Hn: "11", city:"sono", country:"India" }
+    ]},
+    {name:"Choti", email:"choti@g.co", Address:[
+      {Hn: "10", city:"Jamui", country:"India" },
+      {Hn: "12", city:"jhajha", country:"India" },
+      {Hn: "13", city:"khairma", country:"India" },
+      {Hn: "11", city:"sono", country:"India" }
+    ]},
   ]
+  
 
   return (
-    <div className="App" style={{ textAlign: "center" }}>
-      <h1>List with Bootstrap Table</h1>
-<table >
-  <tbody>     
-  <tr>
-    <th scope="col">#</th>
-    <th scope="col">Name</th>
-    <th scope="col">Email</th>
-    <th scope="col">Roll.no</th>
-  </tr>
-  {
-    members.map((item,i)=>
-    <tr key={i} >
-      <th scope="row"></th>
-      <td>{item.name}</td>
-      <td>{item.email}</td>
-      <td>{item.roll}</td>
-    </tr>
-    
-    )
-  }
+    <div className="App">
+<h1>list with nested Array</h1>
+
+<table  >
+  <tbody >
+    <tr  style={{ borderSpacing:"40px", borderCollapse:"separate" }}>     
+   <th>Name</th>
+   <th>email</th>
+   <th>Address</th>
+     </tr>
   </tbody>
 </table>
+{
+  students.map((item)=>
+  <tr key={item} style={{borderSpacing:"40px" , borderCollapse:"separate" }} >
+    <th>{item.name}</th>
+    <th>{item.email}</th>
+    <th>
+      {item.Address.map((data)=>
+      <tr>
+         <th>{data.Hn}</th>
+         <th>{data.city}</th>
+         <th>{data.country}</th>
+      </tr> )
+      }</th>
+  </tr>
+  )
+}
+
+
   </div>
   )
 }

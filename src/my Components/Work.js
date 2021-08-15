b657a07f6f1c0c93e30a6e60d7aae4c3efd6ec26
Anand <anandraj143kick@gmail.com>
2021-08-15 09:35:11 +0530

    troubleShooting 404 Error

--- src/my Components/Work.js
+++ src/my Components/Work.js
@@ -12,8 +12,8 @@ function Work() {
                     <h1>How Much you know me</h1>
                     <hr />
                     <p className="worpa">How much you know me. Built-in NodeJs.</p>
-                    <button className="btn1"> <a className="codlink" href="https://replit.com/@Alligatorr/end-game#index.js"> Code </a> </button>
-                    <button className="btn1">  <a className="codlink" href="https://replit.com/@Alligatorr/end-game?embed=1&output=1#index.js"> Play</a> </button>
+                    <button className="btn1"> <a className="codlink" rel="noreferrer" target="_blank"  href="https://replit.com/@Alligatorr/end-game#index.js"> Code </a> </button>
+                    <button className="btn1">  <a className="codlink" rel="noreferrer" target="_blank"  href="https://replit.com/@Alligatorr/end-game?embed=1&output=1#index.js"> Play</a> </button>
 
                 </section>
 
@@ -21,90 +21,90 @@ function Work() {
                     <h1>Banana Talk</h1>
                     <hr />
                     <p className="worpa"> Try this Banana Talk App. It returns the language in Banana. Only for Entertainment purpose. Built-in javaScript. </p>
-                    <button className="btn2"> <a className="codlink2" href="https://github.com/AlligatorrA/Banana-speak.git">Code</a></button>
-                    <button className="btn2">  <a className="codlink2" href="https://quirky-panini-banana.netlify.app"> ▶️Live Project</a></button>
+                    <button className="btn2"> <a className="codlink2" rel="noreferrer" target="_blank"  href="https://github.com/AlligatorrA/Banana-speak.git">Code</a></button>
+                    <button className="btn2">  <a className="codlink2" rel="noreferrer" target="_blank"  href="https://quirky-panini-banana.netlify.app"> ▶️Live Project</a></button>
                 </section>
 
                 <section className="worsec1" >
                     <h1>Talk British</h1>
                     <hr />
                     <p className="worpa">Are you fan of British accent? Try this App and learn how to speak British. Built-in vanilla js.</p>
-                    <button className="btn1"> <a className="codlink" href="https://github.com/AlligatorrA/Talk-British.git"> Code</a>  </button>
-                    <button className="btn1"> <a className="codlink" href="https://talkbritishbyanand.netlify.app/"> ▶️ Live Project</a> </button>
+                    <button className="btn1"> <a className="codlink" rel="noreferrer" target="_blank"  href="https://github.com/AlligatorrA/Talk-British.git"> Code</a>  </button>
+                    <button className="btn1"> <a className="codlink" rel="noreferrer" target="_blank"  href="https://talkbritishbyanand.netlify.app/"> ▶️ Live Project</a> </button>
                 </section>
 
                 <section className="worsec2">
                     <h1>Emoji Dictionary</h1>
                     <hr />
                     <p className="worpa"> Try this Emoji Dictionary. It returns the Meaning of Emoji. Only for Entertainment purpose. Built-in React js. </p>
-                    <button className="btn2"> <a className="codlink2" href="https://codesandbox.io/s/emoji-dictonary-czbz2">Code</a> </button>
-                    <button className="btn2">  <a className="codlink2" href="https://czbz2.csb.app/"> ▶️Live Project</a> </button>
+                    <button className="btn2"> <a className="codlink2" rel="noreferrer" target="_blank"  href="https://codesandbox.io/s/emoji-dictonary-czbz2">Code</a> </button>
+                    <button className="btn2">  <a className="codlink2" rel="noreferrer" target="_blank"  href="https://czbz2.csb.app/"> ▶️Live Project</a> </button>
                 </section>
 
                 <section className="worsec1" >
                     <h1>Birthday Lucky-or-Not</h1>
                     <hr />
                     <p className="worpa">Lets Play this game Your Birthday Lucky or Not. Built-in pure javaScript.</p>
-                    <button className="btn1">  <a className="codlink" href="https://github.com/AlligatorrA/Birthday-lucky-or-not.git"> Code</a>  </button>
-                    <button className="btn1">  <a className="codlink" href="https://birthday-lucky-or-not.alligatorr.repl.co/"> ▶️ Live Project</a>  </button>
+                    <button className="btn1">  <a className="codlink" rel="noreferrer" target="_blank"  href="https://github.com/AlligatorrA/Birthday-lucky-or-not.git"> Code</a>  </button>
+                    <button className="btn1">  <a className="codlink" rel="noreferrer" target="_blank"  href="https://birthday-lucky-or-not.alligatorr.repl.co/"> ▶️ Live Project</a>  </button>
                 </section>
 
                 <section className="worsec2" >
                     <h1>Good Books</h1>
                     <hr />
                     <p className="worpa">Books you Should read If you are fan of Thrill or Biotechnology or Action. Built using HTML, CSS and React js.</p>
-                    <button className="btn2"> <a className="codlink2" href="https://github.com/AlligatorrA/GoodBooks.git"> Code</a>  </button>
+                    <button className="btn2"> <a className="codlink2" rel="noreferrer" target="_blank"  href="https://github.com/AlligatorrA/GoodBooks.git"> Code</a>  </button>
 
-                    <button className="btn2">  <a className="codlink2" href="https://allligoodbooks.netlify.app/"> ▶️ Live Project</a>  </button>
+                    <button className="btn2">  <a className="codlink2" rel="noreferrer" target="_blank"  href="https://allligoodbooks.netlify.app/"> ▶️ Live Project</a>  </button>
                 </section>
                 <section className="worsec1" >
                     <h1>Favourite Webseries</h1>
                     <hr />
                     <p className="worpa">List of Webseries. I have Watched and You Should Watch too if you are fan of Action or Bio-World of Science-Fiction. Built-in React js .</p>
-                    <button className="btn1"> <a className="codlink" href="https://github.com/AlligatorrA/Fav-Webseries.git"> Code</a>  </button>
-                    <button className="btn1">  <a className="codlink" href="https://fav-webseries.netlify.app"> ▶️ Live Project</a> </button>
+                    <button className="btn1"> <a className="codlink" rel="noreferrer" target="_blank"  href="https://github.com/AlligatorrA/Fav-Webseries.git"> Code</a>  </button>
+                    <button className="btn1">  <a className="codlink" rel="noreferrer" target="_blank"  href="https://fav-webseries.netlify.app"> ▶️ Live Project</a> </button>
                 </section>
                 <section className="worsec2" >
                     <h1>The Originals Fan Game</h1>
                     <hr />
-                    <p className="worpa">This quiz is for "The Originals" webseries fan if you are fan of the great evil "klaus Mikealson" You should must play this quiz.  built-in NodeJs.</p>
-                    <button className="btn2"> <a className="codlink2" href="https://replit.com/@Alligatorr/TheOriginalsFansGame-1#index.js"> Code</a>  </button>
-                    <button className="btn2">  <a className="codlink2" href="https://replit.com/@Alligatorr/TheOriginalsFansGame?embed=1&output=1#index.js"> ▶️ Live Project</a> </button>
+                    <p className="worpa">This quiz is for "The Originals" webseries fan if you are fan of the great evil "klaus Mikealson" You should must play this quiz.  Built-in NodeJs.</p>
+                    <button className="btn2"> <a className="codlink2" rel="noreferrer" target="_blank"  href="https://replit.com/@Alligatorr/TheOriginalsFansGame-1#index.js"> Code</a>  </button>
+                    <button className="btn2">  <a className="codlink2" rel="noreferrer" target="_blank"  href="https://replit.com/@Alligatorr/TheOriginalsFansGame?embed=1&output=1#index.js"> ▶️ Live Project</a> </button>
                 </section>
                 <section className="worsec1" >
                     <h1>Profit and loss</h1>
                     <hr />
-                    <p className="worpa">This app Profit and Loss tells you how much profit or loss you have made along with percentage based on current price, number of Product and the purchase price. built using HTML, CSS, javaScript.</p>
-                    <button className="btn1"> <a className="codlink" href="https://github.com/AlligatorrA/ProfitAndLoss.git"> Code</a>  </button>
-                    <button className="btn1">  <a className="codlink" href="https://alliprofitloss.netlify.app/"> ▶️ Live Project</a>  </button>
+                    <p className="worpa">This app Profit and Loss tells you how much profit or loss you have made along with percentage based on current price, number of Product and the purchase price. Built using HTML, CSS, javaScript.</p>
+                    <button className="btn1"> <a className="codlink" rel="noreferrer" target="_blank"  href="https://github.com/AlligatorrA/ProfitAndLoss.git"> Code</a>  </button>
+                    <button className="btn1">  <a className="codlink" rel="noreferrer" target="_blank" href="https://alliprofitloss.netlify.app/"> ▶️ Live Project</a>  </button>
                 </section>
                 <section className="worsec2" >
                     <h1>Birthday Palindrome or not</h1>
                     <hr />
-                    <p className="worpa">This App will tell you weather you birtdate is palindrome or not along with the  closer palindrome date to your birthday. built with the help of HTMl, css and javaScript.</p>
-                    <button className="btn2"> <a className="codlink2" href="https://replit.com/@Alligatorr/Birthday-Palindrome#index.html "> Code</a>  </button>
-                    <button className="btn2">  <a className="codlink2" href="https://birthday-palindrome.alligatorr.repl.co/"> ▶️ Live Project</a> </button>
+                    <p className="worpa">This App will tell you whether you birtdate is palindrome or not along with the  closer palindrome date to your birthday. Built with the help of HTMl, css and javaScript.</p>
+                    <button className="btn2"> <a className="codlink2" rel="noreferrer" target="_blank"  href="https://replit.com/@Alligatorr/Birthday-Palindrome#index.html "> Code</a>  </button>
+                    <button className="btn2">  <a className="codlink2" rel="noreferrer" target="_blank"  href="https://birthday-palindrome.alligatorr.repl.co/"> ▶️ Live Project</a> </button>
                 </section>
                 <section className="worsec1" >
                     <h1>Cash Register manager</h1>
                     <hr />
-                    <p className="worpa">This is a cash register manager. It indicates the money needed to be returned. built-in javaScript.</p>
-                    <button className="btn1"> <a className="codlink" href="https://github.com/AlligatorrA/Cash-Register-Manager.git"> Code</a>  </button>
-                    <button className="btn1">  <a className="codlink" href="https://allicashmanager.netlify.app/"> ▶️ Live Project</a>  </button>
+                    <p className="worpa">This is a cash register manager. It indicates the money needed to be returned. buit-in javaScript.</p>
+                    <button className="btn1"> <a className="codlink" rel="noreferrer" target="_blank"  href="https://github.com/AlligatorrA/Cash-Register-Manager.git"> Code</a>  </button>
+                    <button className="btn1">  <a className="codlink" rel="noreferrer" target="_blank"  href="https://allicashmanager.netlify.app/"> ▶️ Live Project</a>  </button>
                 </section>
                 <section className="worsec2" >
                     <h1>Fun with Triangle</h1>
                     <hr />
                     <p className="worpa">This is a  Triangle app it can calculate area and it has a quiz part too lets see how much you know of triangle. Built with the help of HTMl, css and javaScript.</p>
-                    <button className="btn2"> <a className="codlink2" href="https://github.com/AlligatorrA/Fun-with-Tringle.git"> Code</a>  </button>
-                    <button className="btn2">  <a className="codlink2" href="https://allifunwithtringle.netlify.app//"> ▶️ Live Project</a> </button>
+                    <button className="btn2"> <a className="codlink2" rel="noreferrer" target="_blank"  href="https://github.com/AlligatorrA/Fun-with-Tringle.git"> Code</a>  </button>
+                    <button className="btn2">  <a className="codlink2"  rel="noreferrer" target="_blank" href="https://allifunwithtringle.netlify.app//"> ▶️ Live Project</a> </button>
                 </section>
                 <section className="worsec1" >
                     <h1>Social Media UI</h1>
                     <hr />
                     <p className="worpa">This is polyTalk. A social Media UI. Built-in HTML and CSS only.</p>
-                    <button className="btn1"> <a className="codlink" href="https://github.com/AlligatorrA/social-media-clone.git"> Code</a>  </button>
-                    <button className="btn1">  <a className="codlink" href="https://socialmediaui.alligatorr.repl.co/"> ▶️ Live Project</a>  </button>
+                    <button className="btn1"> <a className="codlink" rel="noreferrer" target="_blank" href="https://github.com/AlligatorrA/social-media-clone.git"> Code</a>  </button>
+                    <button className="btn1">  <a className="codlink"  rel="noreferrer" target="_blank" href="https://socialmediaui.alligatorr.repl.co/"> ▶️ Live Project</a>  </button>
                 </section>
             </div>
             </div>
